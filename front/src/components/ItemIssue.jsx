041ecf18d1dfef562b392f94188d03707aa9277d
Alejandro Montoya <alejandrom.montoya@udea.edu.co>
2022-02-21 09:31:59 -0500

    elipsis issues

--- front/src/components/ItemIssue.jsx
+++ front/src/components/ItemIssue.jsx
@@ -4,14 +4,14 @@ import { Link } from 'react-router-dom';
 const ItemIssue = ({ issue }) => (
   <Link to={`/Issues/DetallesIssue/${issue.id}`}>
     <div className='flex flex-row items-center w-full h-[50px] px-3 justify-between rounded-md  border-colorNegro border-2 hover:bg-[#d9e0ed] cursor-pointer oveflow-hidden'>
-      <div className=' grid grid-cols-6 relative pr-8 w-full justify-between text-ellipsis overflow-hidden'>
+      <div className=' grid grid-cols-6 gap-2 relative pr-8 w-full justify-between text-ellipsis overflow-hidden'>
         <span className='max-lines-2 text-colorNegro my-auto text-ellipsis overflow-hidden'>
           {issue.description}
         </span>
-        <span className=' text-colorNegro my-auto'>
+        <span className=' text-colorNegro my-auto max-lines-2 text-ellipsis overflow-hidden'>
           {issue.project ? issue.project.name : 'No asignado'}
         </span>
-        <span className=' text-colorNegro my-auto'>
+        <span className=' text-colorNegro my-auto max-lines-1 text-ellipsis overflow-hidden'>
           {issue.developer ? issue.developer.email : 'No asignado'}
         </span>
         <span className=' text-colorNegro my-auto'>{issue.category}</span>
